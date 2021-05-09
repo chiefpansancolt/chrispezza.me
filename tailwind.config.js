afde0d3e0d9847fb39d7d9847d8a3d43acb2a2db
Christopher Pezza <chris.pezza@hey.com>
2021-05-08 22:29:42 -0400

    Base setup

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: '#000',
+      white: '#fff',
+
+      gray: colors.coolGray,
+      purple: colors.purple,
+      yellow: colors.yellow,
+      pink: colors.pink,
+      indigo: colors.indigo,
+      red: colors.red,
+      'light-blue': colors.lightBlue,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
