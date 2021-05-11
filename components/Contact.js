b81cd9efcded553247466f9f8e78bd37c35c4e9a
Christopher Pezza <chris.pezza@hey.com>
2021-05-11 16:32:11 -0400

    Add Contact Section

---
+++ components/Contact.js
@@ -0,0 +1,272 @@
+import { MailIcon } from '@heroicons/react/outline'
+
+export default function Contact() {
+  return (
+    <div className="bg-gray-100" id="contact">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+        <div className="relative bg-white shadow-xl">
+          <h2 className="sr-only">Contact me</h2>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3">
+            <div className="relative overflow-hidden py-10 px-6 bg-blue-700 sm:px-10 xl:p-12">
+              <div className="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
+                <svg
+                  className="absolute inset-0 w-full h-full"
+                  width={343}
+                  height={388}
+                  viewBox="0 0 343 388"
+                  fill="none"
+                  preserveAspectRatio="xMidYMid slice"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
+                    fill="url(#linear1)"
+                    fillOpacity=".1"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="linear1"
+                      x1="254.553"
+                      y1="107.554"
+                      x2="961.66"
+                      y2="814.66"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#fff" />
+                      <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <div
+                className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden"
+                aria-hidden="true"
+              >
+                <svg
+                  className="absolute inset-0 w-full h-full"
+                  width={359}
+                  height={339}
+                  viewBox="0 0 359 339"
+                  fill="none"
+                  preserveAspectRatio="xMidYMid slice"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
+                    fill="url(#linear2)"
+                    fillOpacity=".1"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="linear2"
+                      x1="192.553"
+                      y1="28.553"
+                      x2="899.66"
+                      y2="735.66"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#fff" />
+                      <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <div
+                className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block"
+                aria-hidden="true"
+              >
+                <svg
+                  className="absolute inset-0 w-full h-full"
+                  width={160}
+                  height={678}
+                  viewBox="0 0 160 678"
+                  fill="none"
+                  preserveAspectRatio="xMidYMid slice"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
+                    fill="url(#linear3)"
+                    fillOpacity=".1"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="linear3"
+                      x1="192.553"
+                      y1="325.553"
+                      x2="899.66"
+                      y2="1032.66"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#fff" />
+                      <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <h3 className="text-lg font-medium text-white">Contact Information</h3>
+              <p className="mt-6 text-base text-blue-50 max-w-3xl">
+                Feel free to reach out over any of the below links, socials or contact form to the right.
+              </p>
+              <dl className="mt-8 space-y-6">
+                <dt>
+                  <span className="sr-only">Email</span>
+                </dt>
+                <dd className="flex text-base text-blue-50">
+                  <MailIcon className="flex-shrink-0 w-6 h-6 text-blue-200" aria-hidden="true" />
+                  <span className="ml-3">contact@chris-pezza.com</span>
+                </dd>
+              </dl>
+              <ul className="mt-8 flex space-x-12">
+                <li>
+                  <a className="text-gray-200 hover:text-indigo-400" href="#">
+                    <span className="sr-only">Discord</span>
+                    <svg
+                      width="24"
+                      height="24"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      aria-hidden="true">
+                      <path
+                        d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z" />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-200 hover:text-black" href="#">
+                    <span className="sr-only">GitHub</span>
+                    <svg
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M11.999 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.386.6.11.819-.26.819-.578 0-.284-.01-1.04-.017-2.04-3.337.724-4.042-1.61-4.042-1.61-.545-1.386-1.332-1.755-1.332-1.755-1.09-.744.082-.73.082-.73 1.205.086 1.838 1.238 1.838 1.238 1.07 1.833 2.81 1.304 3.493.996.109-.775.419-1.303.762-1.603C7.145 17 4.343 15.97 4.343 11.373c0-1.31.468-2.382 1.236-3.22-.124-.304-.536-1.524.118-3.176 0 0 1.007-.323 3.3 1.23.956-.266 1.983-.4 3.003-.404 1.02.005 2.046.138 3.005.404 2.29-1.553 3.296-1.23 3.296-1.23.655 1.652.243 2.872.12 3.176.77.838 1.233 1.91 1.233 3.22 0 4.61-2.806 5.624-5.478 5.921.43.37.814 1.103.814 2.223 0 1.603-.015 2.898-.015 3.291 0 .321.217.695.825.578C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12.001-12"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-200 hover:text-light-blue-500" href="#">
+                    <span className="sr-only">Twitter</span>
+                    <svg
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M7.548 22.501c9.056 0 14.01-7.503 14.01-14.01 0-.213 0-.425-.015-.636A10.02 10.02 0 0024 5.305a9.828 9.828 0 01-2.828.776 4.94 4.94 0 002.165-2.724 9.867 9.867 0 01-3.127 1.195 4.929 4.929 0 00-8.391 4.491A13.98 13.98 0 011.67 3.9a4.928 4.928 0 001.525 6.573A4.887 4.887 0 01.96 9.855v.063a4.926 4.926 0 003.95 4.827 4.917 4.917 0 01-2.223.084 4.93 4.93 0 004.6 3.42A9.88 9.88 0 010 20.289a13.941 13.941 0 007.548 2.209"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-200 hover:text-blue-400" href="#">
+                    <span className="sr-only">LinkedIn</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                      <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/* Contact form */}
+            <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
+              <h3 className="text-lg font-medium text-gray-900">Send me a message</h3>
+              <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8" name="contactForm" id="contact-form" netlifyHoneypot="bot-field" data-netlify-recaptcha="true" data-netlify="true">
+                <div className="sm:col-span-2">
+                  <label htmlFor="name" className="block text-sm font-medium text-gray-900">
+                    Name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="given-name"
+                      className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+                    Email
+                  </label>
+                  <div className="mt-1 relative rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <MailIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </div>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="py-3 px-4 pl-10 block w-full shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label htmlFor="subject" className="block text-sm font-medium text-gray-900">
+                    Subject
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="subject"
+                      id="subject"
+                      className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <div className="flex justify-between">
+                    <label htmlFor="message" className="block text-sm font-medium text-gray-900">
+                      Message
+                    </label>
+                    <span id="message-max" className="text-sm text-gray-500">
+                      Max. 500 characters
+                    </span>
+                  </div>
+                  <div className="mt-1">
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows={4}
+                      className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                      aria-describedby="message-max"
+                      defaultValue={''}
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2 sm:flex sm:justify-between">
+                  <div data-netlify-recaptcha="true"></div>
+                  <button
+                    type="submit"
+                    className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:w-auto"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
