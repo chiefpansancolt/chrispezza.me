4aa1122c772a45ed8aae6373d98ffa4ca6d93c43
Christopher Pezza <chris.pezza@hey.com>
2021-05-12 22:55:05 -0400

    add additional pages to check for purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   // mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.js', './components/**/*.js', './data/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
