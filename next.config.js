22e34f959bfd0eb4f179ef4eba0e6745e67defd6
Christopher Pezza <chris.pezza@hey.com>
2021-05-15 12:07:02 -0400

    add file-loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 module.exports = {
-  future: {
-    webpack5: true,
+  experimental: {
+    modern: true,
   },
   target: 'serverless',
+  webpack(config, options) {
+    if (!options.dev) {
+      options.defaultLoaders.babel.options.cache = false
+    }
+
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|webp)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    })
+
+    return config
+  }
 }
