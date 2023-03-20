bd01a427ed4718c4bed032d49fe728e5ed0a5dd9
Christopher Pezza <chris.pezza@hey.com>
2023-03-19 22:19:16 -0400

    clean up link

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -8,10 +8,11 @@ export default function Certifications() {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="flex justify-center mb-10 lg:mb-0">
           <h4 className="text-xl">
-            <Link href="https://trailhead.salesforce.com/credentials/certification-detail-print?searchString=6G3owgggG1JJQtqmZEXXJnwkqVnzAiE8e2KxdsFb3G04A8YeYm6DcQpbm4/fVDxp">
-              <span className="border-solid border-b-4 border-blue-500 uppercase tracking-widest font-bold cursor-pointer">
-                Salesforce Certifications
-              </span>
+            <Link
+              href="https://trailhead.salesforce.com/credentials/certification-detail-print?searchString=6G3owgggG1JJQtqmZEXXJnwkqVnzAiE8e2KxdsFb3G04A8YeYm6DcQpbm4/fVDxp"
+              className="border-solid border-b-4 border-blue-500 uppercase tracking-widest font-bold cursor-pointer"
+            >
+              Salesforce Certifications
             </Link>
           </h4>
         </div>
