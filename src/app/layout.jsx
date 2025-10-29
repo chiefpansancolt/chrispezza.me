3546d1767a9af0e0d3ad3f92784fcc2b2f954718
Christopher Pezza <chris.pezza@hey.com>
2025-10-29 12:09:59 -0400

    update deps

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,10 +5,10 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Christopher Pezza | Architect, Developer, Husband, Father, Streamer',
+  title: 'Christopher Pezza | Architect, Developer, Husband, Father',
   description:
     'Christopher Pezza Portfolio site showcasing Work, Education, Experience, and Skills.',
-  keywords: 'chris pezza, Chiefpansancolt, Porfolio, Development, Salesforce',
+  keywords: 'chris pezza, Chiefpansancolt, Portfolio, Development, Salesforce',
 }
 
 export default function RootLayout({ children }) {
@@ -20,21 +20,6 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
-
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-            `,
-          }}
-        />
       </head>
       <body className={inter.className}>
         {children}
